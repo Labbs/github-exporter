8b9128785147d3dfd790d473e7ec535dba81fdd7
Labbs <moutonjeremy@labbs.fr>
2023-04-08 08:23:12 +0200

    add logger

--- prom/common.go
+++ prom/common.go
@@ -1,8 +1,12 @@
 package prom
 
-import "github.com/google/go-github/v51/github"
+import (
+	"github.com/google/go-github/v51/github"
+	"github.com/rs/zerolog"
+)
 
 var (
 	repositories []string
 	workflows    map[string]map[int64]github.Workflow
+	Logger       zerolog.Logger
 )
