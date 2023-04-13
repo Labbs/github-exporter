a3c8bf0fa062e4a8383aa604664fa61b61f78762
Labbs <moutonjeremy@labbs.fr>
2023-04-14 00:07:40 +0200

    add test if the cron scheduler return an error during the initialization

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -25,16 +25,34 @@ func App(version string) Application {
 
 	prom.Logger = app.Logger
 
-	app.CronScheduler.Every(config.Github.RefreshInterval*5).Second().Do(prom.GithubFetcher, app.GithubClient)
-
-	if config.Github.EnterpriseName != "" {
-		app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetRunnersEnterpriseFromGithub, app.GithubClient)
+	_, err := app.CronScheduler.Every(config.Github.RefreshInterval*5).Second().Do(prom.GithubFetcher, app.GithubClient)
+	if err != nil {
+		app.Logger.Fatal().Err(err).Msg("Error scheduling GtihubFetcher jobs")
 	}
 
-	app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetBillableFromGithub, app.GithubClient)
-	app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetRunnersFromGhithub, app.GithubClient)
-	app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetRunnersOrganizationFromGithub, app.GithubClient)
-	app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetWorkflowRunsFromGithub, app.GithubClient)
+	if config.Github.EnterpriseName != "" {
+		_, err = app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetRunnersEnterpriseFromGithub, app.GithubClient)
+		if err != nil {
+			app.Logger.Fatal().Err(err).Msg("Error scheduling GetRunnersEnterpriseFromGithub jobs")
+		}
+	}
+
+	_, err = app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetBillableFromGithub, app.GithubClient)
+	if err != nil {
+		app.Logger.Fatal().Err(err).Msg("Error scheduling GetBillableFromGithub jobs")
+	}
+	_, err = app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetRunnersFromGhithub, app.GithubClient)
+	if err != nil {
+		app.Logger.Fatal().Err(err).Msg("Error scheduling GetRunnersFromGhithub jobs")
+	}
+	_, err = app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetRunnersOrganizationFromGithub, app.GithubClient)
+	if err != nil {
+		app.Logger.Fatal().Err(err).Msg("Error scheduling GetRunnersOrganizationFromGithub jobs")
+	}
+	_, err = app.CronScheduler.Every(config.Github.RefreshInterval).Second().Do(prom.GetWorkflowRunsFromGithub, app.GithubClient)
+	if err != nil {
+		app.Logger.Fatal().Err(err).Msg("Error scheduling GetWorkflowRunsFromGithub jobs")
+	}
 
 	return *app
 }
